ebfdd7ebd81646159a468e1e4d71c88d4069e408
Junior <loraluviel@gmail.com>
2018-10-17 08:17:29 +0200

    js nie do konca dziala

---
+++ Lab1/rozmiar.js
@@ -0,0 +1,11 @@
+const btn1 = document.querySelector(".b1");
+const btn2 = document.querySelector(".b2");
+const kck = document.querySelector("img.kucko");
+//var kck1 = document.getElementById('img.kucko');
+
+btn1.addEventListener('click', function () {
+
+    kck1.height = 600;
+    kck1.width = 600;
+
+});
